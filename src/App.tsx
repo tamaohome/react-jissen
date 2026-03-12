acfa2f64bf6508eb02332b949f496c5c30f01f0d
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-03-12 13:25:51 +0900

    パスエイリアスを設定

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { MyHello } from "./components/MyHello";
+import { MyHello } from "@/components/MyHello";
 
 function App() {
   const [count, setCount] = useState(0);
