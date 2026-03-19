fbb9a73eb4f98caa956239cf3ac6b33587178499
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-03-19 09:35:44 +0900

    ボタンのスタイルを調整 (UI/Button)

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -9,7 +9,7 @@ function cn(...inputs: ClassValue[]) {
 
 const variantClasses = {
   normal: clsx("bg-slate-500 text-white data-hover:bg-slate-600"),
-  info: clsx("bg-blue-600 text-white data-hover:bg-blue-700"),
+  info: clsx("bg-sky-600 text-white data-hover:bg-sky-700"),
   success: clsx("bg-emerald-600 text-white data-hover:bg-emerald-700"),
   warning: clsx("bg-yellow-500 data-hover:bg-yellow-600 text-gray-900"),
   danger: clsx("bg-red-500 data-hover:bg-red-600 text-white"),
@@ -38,7 +38,7 @@ export function Button({
       className={cn(
         "inline-flex items-center justify-center rounded-sm px-5 py-2 shadow-md transition-all",
         !disabled && "cursor-pointer active:scale-95",
-        disabled ? "bg-slate-400 text-white" : variantClasses[variant],
+        disabled ? "bg-slate-300 text-white" : variantClasses[variant],
         className,
       )}
     >
